07c15214eb1907d1b36f0b4db594a5a057a95890
ipiup <caro.hamery@gmail.com>
2023-04-04 14:25:21 +0200

    functions

--- src/p01_load_data.m
+++ src/p01_load_data.m
@@ -1,32 +1,31 @@
-function p_01_load_data(settings)
+function p01_load_data(settings)
 % vCH
 
-
-if s.todo.load_data
+if settings.todo.load_data
     
     % find input data folders
-    folders = dir(s.path.data); % get folder content of (input) data folder
+    folders = dir(settings.path.data); % get folder content of (input) data folder
     folders = folders(contains({folders.name},'sub')); % get only the content containing 'NDAR'
     
     %% Reading Files
-    if s.multipleSessions == true
+    if settings.multipleSessions == true
         %Case : Multiple Sessions, loop over each participant and each sessions
         for i=1:length(folders) %each participant
             pFolder = dir([folders(i).folder, filesep, folders(i).name]);
             sFolders = pFolder(contains({pFolder.name},'ses'));
-            for j = 1:length(sFolders)%s.nSessions %each session
+            for j = 1:length(sFolders)%settings.nSessions %each session
                 sFile = sFolders(j);
-                outputfile = [s.path.gfp,folders(i).name,filesep];
-                disp(['p01: ', 'participant: ',num2str(i), '/ ', num2str(length(folders)),' session: ',num2str(j),'/ ',num2str(length(sFolders))]);
-                pl_load_data(sFile,outputfile, s); % perform the MSA-specific pre-processing on each session of each participant
+                outputfile = [settings.path.gfp,folders(i).name,filesep];
+                disp(['p01 Load Data: ', 'participant: ',num2str(i), '/ ', num2str(length(folders)),' session: ',num2str(j),'/ ',num2str(length(sFolders))]);
+                pl_load_data(sFile,outputfile, settings); % perform the MSA-specific pre-processing on each session of each participant
             end
         end
     else
         %1 Session:
         %loop over folders resp. all subjects
         for i=1:length(folders)
-            disp(['p01: ', num2str(i), '/ ', num2str(length(folders))])
-            pl_load_data(folders(i), s.path.gfp, s); % perform the MSA-specific pre-processing
+            disp(['p01 Load Data: ', num2str(i), '/ ', num2str(length(folders))])
+            pl_load_data(folders(i), settings.path.gfp, settings); % perform the MSA-specific pre-processing
         end
     end
     
