773f548049141bc31bb0c39ebddb72fa780921d1
ipiup <caro.hamery@gmail.com>
2023-04-13 16:24:30 +0200

    ameliorations

--- src/p01_load_data.m
+++ src/p01_load_data.m
@@ -1,33 +1,26 @@
 function p01_load_data(settings)
-% vCH
-
 if settings.todo.load_data
     
     % find input data folders
     folders = dir(settings.path.data); % get folder content of (input) data folder
-    folders = folders(contains({folders.name},'sub')); % get only the content containing 'NDAR'
+    folders = folders(~contains({folders.name},'.')); % ignore parent folders
     
-    dir([settings.path.data,**]
-    folders = folders(contains({folders.name},'*eeg.mat'))
-    %% Reading Files
-    if settings.levels == 3 %settings.multipleSessions == true% First Level : Sessions
-        %Case : Multiple Sessions, loop over each participant and each sessions
+    if length(settings.levels) == 3 % First Level : Sessions
         for i=1:length(folders) %each participant
-            pFolder = dir([folders(i).folder, filesep, folders(i).name]);
-            sFolders = pFolder(contains({pFolder.name},'ses'));
-            for j = 1:length(sFolders)%settings.nSessions %each session
-                sFile = sFolders(j);
+            fp_folder = dir([folders(i).folder, filesep, folders(i).name]);
+            fp_folder = fp_folder(~contains({fp_folder.name},'.')); % ignore parent folders
+            for j = 1:length(fp_folder) %each session
+                fp_file = fp_folder(j);
                 outputfile = [settings.path.gfp,folders(i).name,filesep];
-                disp(['p01 Load Data: ', 'participant: ',num2str(i), '/ ', num2str(length(folders)),' session: ',num2str(j),'/ ',num2str(length(sFolders))]);
-                pl_load_data(sFile,outputfile, settings); % perform the MSA-specific pre-processing on each session of each participant
+                disp(['p01 Load Data: ', 'participant: ',num2str(i), '/ ', num2str(length(folders)),' session: ',num2str(j),'/ ',num2str(length(fp_folder))]);
+                pl_load_data(fp_file,outputfile, settings);
             end
         end
-    else % First level : Participants
+      else % First level : Participants
         for i=1:length(folders)
             disp(['p01 Load Data: ', num2str(i), '/ ', num2str(length(folders))])
-            pl_load_data(folders(i), settings.path.gfp, settings); % perform the MSA-specific pre-processing
+            pl_load_data(folders(i), settings.path.gfp, settings);
         end
-    end
-    
+    end   
 end
 end
--- src/p02_gfp_peaks.m
+++ src/p02_gfp_peaks.m
@@ -1,31 +1,26 @@
 function p02_gfp_peaks(settings)
-% vCH
 %% GFP PEAKS
 if settings.todo.microstates_gfppeaks
     
-    outputfolder = [settings.path.gfp];
+    outputfolder = [settings.path.gfp]; % gfp peaks folder 
+    folders = dir(settings.path.gfp); 
+    folders = folders(~contains({folders.name},'.')); % ignore parent folders
     if settings.multipleSessions == true
-        folders = dir(settings.path.gfp); % if multiple sessions : Gfp Peaks detection on each session
-        folders = folders(contains({folders.name},'sub'));
         for i=1:length(folders)
             outputparticipantfolder = [outputfolder,folders(i).name];
-            pFolder = dir([folders(i).folder, filesep, folders(i).name]);
-            sFolders = pFolder(contains({pFolder.name},'ses'));
-            for j = 1:length(sFolders)%settings.nSessions
-                outputsessionfolder = [outputparticipantfolder, filesep, sFolders(j).name];
-                sFile = sFolders(j);
-                disp(['p02 GFP Peaks: ', 'participant: ',num2str(i), filesep, num2str(length(folders)),' session: ',num2str(j),filesep,num2str(length(sFolders))]);
-                pl_microstates_gfppeaks(sFile, outputsessionfolder, settings); %input & output folder = gfp-folder with subfolder for each session of each participant
+            fp_folder = dir([folders(i).folder, filesep, folders(i).name]);
+            fp_folder = fp_folder(~contains({fp_folder.name},'.')); % ignore parent folders
+            for j = 1:length(fp_folder)
+                outputsessionfolder = [outputparticipantfolder, filesep, fp_folder(j).name];
+                fp_file = fp_folder(j);
+                disp(['p02 GFP Peaks: ', 'participant: ',num2str(i), filesep, num2str(length(folders)),' session: ',num2str(j),filesep,num2str(length(fp_folder))]);
+                pl_microstates_gfppeaks(fp_file, outputsessionfolder, settings); %input & output folder = gfp-folder with subfolder for each session of each participant
             end
         end
     else
-        folders = dir(settings.path.gfp); % if single session : Gfp Peaks detection on each participant
-        folders = folders(contains({folders.name},'sub'));
-        % Signle session
-        %loop over folders resp. subjects
-        for i=1:length(folders)
+        for i=1:length(folders)% Single session
             outputparticipantfolder = [outputfolder,filesep,folders(i).name];
-            disp(['p02 GFP Peaks: ', num2str(i), '/ ', num2str(length(folders))])
+            disp(['p02 GFP Peaks: ', num2str(i), filesep, num2str(length(folders))])
             pl_microstates_gfppeaks(folders(i), outputparticipantfolder, settings); %input & output folder = gfp-folder for each participant
         end
     end
