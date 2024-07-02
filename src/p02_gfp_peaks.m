f5aa28be423ce53be6aa1db188f0a28e1777e20c
ipiup <33489434+ipiup@users.noreply.github.com>
2024-07-02 17:15:20 +0200

    gfp peaks merge

--- src/p02_gfp_peaks.m
+++ src/p02_gfp_peaks.m
@@ -14,6 +14,45 @@ if settings.todo.microstates_gfppeaks
         outputfolder = folders(i).folder;
         pl_microstates_gfppeaks(inputfolder, outputfolder, settings); %input & output folder = gfp-folder for each participant
     end
+    
+    firstlevel = char(settings.levels(1));
+    switch firstlevel
+        case "group"            
+            fp_all_gfp = dir([settings.path.gfp,filesep, '**',filesep,'gfppeaks.mat']); % all gfp files of all subjects and all sessions
+            concat_gfp.CEEG = []; % empty concat gfp            
+            for g = 1:length(fp_all_gfp)
+                tmp_gfp = load([fp_all_gfp(g).folder,filesep,fp_all_gfp(g).name]);
+                concat_gfp.CEEG = horzcat(concat_gfp.CEEG, tmp_gfp.CEEG);
+            end
+            fp_delete = dir(settings.path.gfp);
+            fp_delete =  fp_delete(~contains({fp_delete.name},{'.','..','group'})); 
+            for d = 1:length(fp_delete)
+                rmdir([fp_delete(d).folder,filesep,fp_delete(d).name,filesep],'s');
+            end
+            save([settings.path.gfp,filesep,'group',filesep,'gfppeaks.mat'],'-struct','concat_gfp'); % save EEG only
+            movefile([settings.path.gfp,filesep,'group',filesep,'group_eegdata.mat'],[settings.path.gfp,filesep,'group',filesep,'eegdata.mat']);
+        case "participant"
+            if settings.multipleSessions
+                fp_participants = dir(settings.path.gfp);
+                fp_participants  = fp_participants(~contains({fp_participants.name},'.')); % ignore parent folders
+                for p = 1:length(fp_participants)
+                    fp_p  = [fp_participants(p).folder,filesep,fp_participants(p).name];
+                    fp_all_gfp = dir([fp_p,filesep, '**',filesep,'gfppeaks.mat']); % all gfp files of all subjects and all sessions
+                    concat_gfp.CEEG = []; % empty concat gfp            
+                    for g = 1:length(fp_all_gfp)
+                        tmp_gfp = load([fp_all_gfp(g).folder,filesep,fp_all_gfp(g).name]);
+                        concat_gfp.CEEG = horzcat(concat_gfp.CEEG, tmp_gfp.CEEG);
+                    end
+                    fp_delete = dir(fp_p);
+                    fp_delete =  fp_delete(~contains({fp_delete.name},{'.','..','.mat'})); 
+                    for d = 1:length(fp_delete)
+                        rmdir([fp_delete(d).folder,filesep,fp_delete(d).name,filesep],'s');
+                    end
+                    save([fp_p,filesep,'gfppeaks.mat'],'-struct','concat_gfp'); % save EEG only    
+                    movefile([settings.path.gfp,fp_participants(p).name,filesep,'participant_eegdata.mat'],[settings.path.gfp,filesep,fp_participants(p).name,filesep,'eegdata.mat']);
+                end
+            end
+    end
 end
 
 % if settings.todo.microstates_gfppeaks
